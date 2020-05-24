240861f73184c412c554fa475eb10e2e490f85c0
Emirayka <emirayka@protonmail.com>
2020-05-24 21:35:58 +0300

    Added smth.

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,47 @@
+use std::fmt::Debug;
+
+use float_cmp::ApproxEq;
 use pretty_assertions::assert_eq;
 use pretty_assertions::assert_ne;
 
-#[macro_export]
-macro_rules! nia_assert {
-    ($what:expr) => {
-        assert!($what);
-    }
+pub fn nia_assert_equal<T>(v1: T, v2: T)
+    where T: PartialEq + Eq + Debug
+{
+    assert_eq!(v1, v2);
 }
 
-#[macro_export]
-macro_rules! nia_assert_equal {
-    ($left:expr, $right:expr) => {
-        assert_eq!($left, $right);
-    }
+pub fn nia_assert_nequal<T>(v1: T, v2: T)
+    where T: PartialEq + Eq + Debug
+{
+    assert_ne!(v1, v2);
 }
 
-#[macro_export]
-macro_rules! nia_assert_nequal {
-    ($left:expr, $right:expr) => {
-        assert_ne!($left, $right);
-    }
+pub fn nia_assert_fequal(v1: f64, v2: f64)
+{
+    assert!(v1.approx_eq(v2, (0.0, 2)));
 }
 
-#[macro_export]
-macro_rules! nia_assert_is_ok {
-    ($what:expr) => {
-        assert!($what.is_ok());
-    }
+pub fn nia_assert_fnequal(v1: f64, v2: f64)
+{
+    assert!(!v1.approx_eq(v2, (0.0, 2)));
 }
 
-#[macro_export]
-macro_rules! nia_assert_is_err {
-    ($what:expr) => {
-        assert!($what.is_err());
-    }
+pub fn nia_assert(result: bool) {
+    assert!(result);
+}
+
+pub fn nia_assert_is_ok<V, E>(result: &Result<V, E>) {
+    assert!(result.is_ok())
+}
+
+pub fn nia_assert_is_err<V, E>(result: &Result<V, E>) {
+    assert!(result.is_err())
+}
+
+pub fn nia_assert_is_true(value: bool) {
+    assert_eq!(true, value);
+}
+
+pub fn nia_assert_is_false(value: bool) {
+    assert_eq!(false, value);
 }
